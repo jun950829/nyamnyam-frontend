26e0b77bf2ba1fc00fe90d16be300a91f2c8a0b4
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-11 15:55:40 +0900

    loginform 수정

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -1,25 +1,40 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Input from "@/components/commons/inputs/Input";
+import * as yup from "yup";
 import { useRouter } from "next/navigation";
 
-const LoginForm = () => {
-  
-  const idRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const [error, setError] = useState("");
-  const router = useRouter();
+const schema = yup.object().shape({
+  id: yup
+    .string()
+    .required("Id를 입력해주세요"),
+  password: yup.string().required("Password를 입력해주세요")
+});
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!idRef.current?.value || !passwordRef.current?.value) {
-      setError("아이디 또는 비밀번호를 입력해주세요");
-      return;
-    }
-    setError("");
-    alert(`Login successful! ID: ${idRef.current.value}`);
-  };
+const LoginForm = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+    const onSubmit = (data: {id:string, password: string}) => {
+      setLoading(true);
+  
+      // alert(`일단 됐다고 친다`);
+      setLoading(false);
+      console.log(data);
+      router.push("/");
+  
+    };
 
   const goToSignUp = () => {
     router.push("login/signup")
@@ -27,13 +42,13 @@ const LoginForm = () => {
 
   return (
     <>
-    <form onSubmit={handleSubmit} className="p-4 max-w-md mx-auto space-y-4">
-      <Input ref={idRef} label="아이디" type="text" placeholder="아이디" />
-      <Input ref={passwordRef} label="비밀번호" type="password" placeholder="비밀번호" />
-      {error && <p className="text-sm text-red-500">{error}</p>}
-      <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-        로그인
+    <form onSubmit={handleSubmit(onSubmit)} className="p-4 max-w-md mx-auto space-y-4">
+      <Input label="아이디" {...register("id")} error={errors.id?.message} placeholder="아이디" />
+      <Input label="비밀번호" type="password" {...register("password")} error={errors.password?.message} placeholder="비밀번호" />
+      <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" disabled={loading}>
+        {loading ? "로그인 중..." : "로그인"}
       </button>
+
       <button type="button" className="w-full p-2 bg-gray-500 text-white rounded-md hover:bg-blue-600" onClick={goToSignUp}>
         회원가입
       </button>
