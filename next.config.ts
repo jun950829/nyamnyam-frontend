ef9d8fa48b9b99c285deb5ddd371ebe84fa7ab8c
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-12 01:34:42 +0900

    config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: "/api/:path*", // Next.js에서 API 요청을 가로채서 백엔드로 전달
-        destination: "https://nyamnyam.laterre.dev/api/:path*", // 실제 백엔드 주소
-      },
-    ];
-  },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
