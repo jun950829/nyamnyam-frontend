a6250b292dd1319fb52ce83fabf1896eec060fbe
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-12 01:24:52 +0900

    config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Next.js에서 API 요청을 가로채서 백엔드로 전달
+        destination: "https://nyamnyam.laterre.dev/api/:path*", // 실제 백엔드 주소
+      },
+    ];
+  },
 };
 
 export default nextConfig;
