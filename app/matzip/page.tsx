171d512a11b4ec4f32f175edbd5b667cbb0ec75f
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-12 07:04:48 +0900

    글쓰기 추가

--- app/matzip/page.tsx
+++ app/matzip/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
+import Btn from "@/components/commons/buttons/Btn";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
 
@@ -22,24 +23,29 @@ export default function MatZip () {
 
     return(
       <>
-        {matzipList.map((posts: any, index) => {
-          return (
-            <div key={index} onClick={() => {
-              router.push(`/matzip/${posts.id}`);
-            }}>
-              <p>{posts.title}</p>
-              {posts.media_data ? (
-            <img
-              src={`data:image/png;base64,${posts.media_data}`}
-              alt="게시글 이미지"
-              className="mt-4 w-96 h-auto object-cover"
-            />
-          ) : (
-            <p className="mt-4 text-gray-500">이미지가 없습니다.</p>
-          )}
-            </div>
-          )
-        })}
+        <div className="p-4">
+          <div className="flex flex-row justify-between items-center">
+            <p>맛집 공유 POST</p> <Btn label="글쓰기" onClick={() => router.push("matzip/write")} />
+          </div>
+          {matzipList.map((posts: any, index) => {
+            return (
+              <div key={index} onClick={() => {
+                router.push(`/matzip/${posts.id}`);
+              }}>
+                <p>{posts.title}</p>
+                {posts.media_data ? (
+              <img
+                src={`data:image/png;base64,${posts.media_data}`}
+                alt="게시글 이미지"
+                className="mt-4 w-96 h-auto object-cover"
+              />
+            ) : (
+              <p className="mt-4 text-gray-500">이미지가 없습니다.</p>
+            )}
+              </div>
+            )
+          })}
+        </div>
       </>
     )
 
