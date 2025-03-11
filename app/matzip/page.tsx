fbe1aa587ff3694b6d95dc544b329946241a5794
admin <admin@adminui-MacBookAir.local>
2025-03-12 07:36:51 +0900

    디자인 추가 중

--- app/matzip/page.tsx
+++ app/matzip/page.tsx
@@ -23,25 +23,30 @@ export default function MatZip () {
 
     return(
       <>
-        <div className="p-4">
-          <div className="flex flex-row justify-between items-center">
-            <p>맛집 공유 POST</p> <Btn label="글쓰기" onClick={() => router.push("matzip/write")} />
+        <div className="w-8/10 p-4 min-h-600 mx-auto">
+          <div className="flex flex-row justify-between items-center mt-4">
+            <p className="text-2xl text-bold">맛집 공유 POST</p> <Btn label="글쓰기" onClick={() => router.push("matzip/write")} />
           </div>
           {matzipList.map((posts: any, index) => {
             return (
-              <div key={index} onClick={() => {
+              <div key={index} className="w-full max-w-sm shadow-lg rounded-2xl overflow-hidden hover:shadow-2xl transition-shadow"onClick={() => {
                 router.push(`/matzip/${posts.id}`);
               }}>
-                <p>{posts.title}</p>
-                {posts.media_data ? (
+              {posts.media_data ? (
               <img
                 src={`data:image/png;base64,${posts.media_data}`}
                 alt="게시글 이미지"
-                className="mt-4 w-96 h-auto object-cover"
+                className="w-full h-48 object-cover"
               />
-            ) : (
-              <p className="mt-4 text-gray-500">이미지가 없습니다.</p>
-            )}
+              ) : (
+                <p className="mt-4 text-gray-500">이미지가 없습니다.</p>
+              )}
+
+              <p className="text-xl font-bold mb-2">{posts.title}</p>
+              <div className="flex items-center text-gray-500 text-sm mb-4">
+                <span className="mr-4">by {posts.nickname}</span>
+                <span>👍 {posts.likes}</span>
+              </div>
               </div>
             )
           })}
