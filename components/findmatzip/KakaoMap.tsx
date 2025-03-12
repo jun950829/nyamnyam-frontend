4607a0239c793cc9fcb00424498107cfa7faa220
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-12 13:04:03 +0900

    kakao change

--- components/findmatzip/KakaoMap.tsx
+++ components/findmatzip/KakaoMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "react-kakao-maps-sdk";
 
 type Location = {
@@ -12,39 +12,40 @@ interface KakaoMapProps {
     location: Location[];
 }
 
-const KakaoMap = ({location} : KakaoMapProps) => {
-    const [mounted, setMounted] = useState(false);
+export default function KakaoMap ({location} : KakaoMapProps) {
 
     useEffect(()=>{
     	// 1. 카카오 지도 초기화
-        if(kakao === undefined)
-            return ;
-        kakao?.maps.load(() => {
+            if (typeof window !== "undefined" && window.kakao) {
+              // ✅ 카카오 SDK가 로드되었는지 확인
+            const script = document.createElement("script");
+            script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false`;
+            script.async = true;
+            script.onload = () => {
+                window.kakao.maps.load(() => {
+                    const container = document.getElementById("map");
+        
+                    const options = {
+                        center: location.length !== 0 ? new kakao.maps.LatLng(location[0]?.y, location[0]?.x) : new kakao.maps.LatLng(127.0292884713586, 37.49554428487904),
+                        level: 3,
+                    };
+                    
+                    // 맵 생성
+                    const map = new kakao.maps.Map(container as HTMLElement, options);
+                    
+                    const marker = new kakao.maps.Marker({
+                        map: map,
+                        position: location.length !== 0 ? new kakao.maps.LatLng(location[0]?.y, location[0]?.x) : new kakao.maps.LatLng(127.0292884713586, 37.49554428487904),
+                    });
+                    
+                    marker.setMap(map); 
+                })
+        // kakao?.maps.load(() => {
         	// 2. 지도 생성 및 설정
-            const container = document.getElementById("map");
-
-            const options = {
-                center: location.length !== 0 ? new kakao.maps.LatLng(location[0]?.y, location[0]?.x) : new kakao.maps.LatLng(127.0292884713586, 37.49554428487904),
-                level: 3,
-            };
-            
-            // 맵 생성
-            const map = new kakao.maps.Map(container as HTMLElement, options);
-            
-            const marker = new kakao.maps.Marker({
-                map: map,
-                position: location.length !== 0 ? new kakao.maps.LatLng(location[0]?.y, location[0]?.x) : new kakao.maps.LatLng(127.0292884713586, 37.49554428487904),
-            });
-
-            
-            marker.setMap(map); 
-
-        });
-    }, [location, mounted]);
-
-    useEffect(() => {
-        setMounted(true);
-    }, [])
+            document.head.appendChild(script);
+        }
+    }
+    }, []);
 
     return (
         <>
@@ -52,5 +53,3 @@ const KakaoMap = ({location} : KakaoMapProps) => {
         </>
     )
 }
-
-export default KakaoMap;
